6d51bc7f9beb3f221a397bbe78772d5b7ee534e0
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-17 19:27:40 +0500

    Fix formatting and remove unnecessary line breaks

--- main.py
+++ main.py
@@ -8,7 +8,5 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot ishlayapti 🚀")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
-
 app.run_polling()
