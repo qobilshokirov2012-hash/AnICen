fdb19abc78a5cacd33312ea5ee4b786f5634f4a6
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-26 08:53:43 +0500

    Refactor emoji IDs and add translation function

--- bot.py
+++ bot.py
@@ -5,22 +5,40 @@ from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from motor.motor_asyncio import AsyncIOMotorClient
 
+# Railway Variables
 TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URL = os.getenv("MONGO_URL")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# MongoDB ulanishi
 client = AsyncIOMotorClient(MONGO_URL)
 db = client.anime_bot_db
 users_collection = db.users
 
-# Siz bergan Premium Emoji IDlaridan namunalar
-EMOJI_STAR = "6039646800489486634"
-EMOJI_FIRE = "5890883384057533697"
-EMOJI_DIAMOND = "6039853100653612987"
-EMOJI_SEARCH = "6033125983572201397"
+# --- SIZ BERGAN PREMIUM EMOJI IDLARI ---
+E_NOM = "5886473311637999700"          # Nom yozuvi uchun
+E_SEARCH = "5888620056551625531"       # Nom qidiruv uchun
+E_SIFAT = "5850392884817172292"        # Sifat (Reyting o'rnida ishlatsa bo'ladi)
+E_TAG = "5890883384057533697"          # Teg (Annotatsiya uchun)
+E_USER = "5938196735200333756"         # Foydalanuvchi
+E_SHARE = "6039539366177541657"        # Ulashish
+E_SILENT = "6039853100653612987"       # Ovozsiz
 
+# Inglizcha matnni o'zbekchaga tarjima qilish
+async def translate_to_uz(text):
+    if not text:
+        return "Ma'lumot topilmadi."
+    url = f"https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=uz&dt=t&q={text}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.json()
+                return "".join([i[0] for i in data[0]])
+            return text
+
+# Anime qidirish funksiyasi
 async def search_anime(query):
     url = f"https://api.jikan.moe/v4/anime?q={query}&limit=1"
     async with aiohttp.ClientSession() as session:
@@ -36,26 +54,27 @@ async def start_cmd(message: types.Message):
     user_id = message.from_user.id
     first_name = message.from_user.first_name
     
+    # Foydalanuvchini bazaga tekshirish/qo'shish
     user = await users_collection.find_one({"user_id": user_id})
-    
-    # Premium emoji bilan salomlashish (HTML formatda)
-    welcome_emoji = f'<tg-emoji emoji-id="{EMOJI_STAR}">✨</tg-emoji>'
+    user_emoji = f'<tg-emoji emoji-id="{E_USER}">👤</tg-emoji>'
     
     if not user:
         await users_collection.insert_one({
-            "user_id": user_id,
+            "user_id": user_id, 
             "first_name": first_name,
             "username": message.from_user.username
         })
-        text = f"<b>Salom, {first_name}!</b> {welcome_emoji}\n\n<b>Sizning ID:</b> <code>{user_id}</code>\nAnime nomini yuboring!"
+        text = f"{user_emoji} <b>Salom, {first_name}!</b>\n\nBotimizga xush kelibsiz! Anime nomini yozing va men sizga ma'lumot topib beraman."
     else:
-        text = f"<b>Qayta ko'rishganimizdan xursandman!</b> {welcome_emoji}\nAnime nomini yuboring!"
+        text = f"{user_emoji} <b>Qayta ko'rishganimizdan xursandman, {first_name}!</b>"
 
     await message.answer(text, parse_mode="HTML")
 
 @dp.message(F.text)
 async def handle_anime_search(message: types.Message):
-    search_emoji = f'<tg-emoji emoji-id="{EMOJI_SEARCH}">🔎</tg-emoji>'
+    if message.text.startswith("/"): return
+
+    search_emoji = f'<tg-emoji emoji-id="{E_SEARCH}">🔎</tg-emoji>'
     msg = await message.answer(f"Qidiryapman... {search_emoji}", parse_mode="HTML")
     
     anime = await search_anime(message.text)
@@ -64,23 +83,35 @@ async def handle_anime_search(message: types.Message):
         title = anime['title']
         score = anime.get('score', 'Noma’lum')
         image_url = anime['images']['jpg']['large_image_url']
+        synopsis_en = anime.get('synopsis', 'Ma\'lumot yo\'q.')
         
-        # Sarlavhaga brilliant emoji qo'shamiz
-        diamond = f'<tg-emoji emoji-id="{EMOJI_DIAMOND}">💎</tg-emoji>'
-        fire = f'<tg-emoji emoji-id="{EMOJI_FIRE}">🔥</tg-emoji>'
+        # O'zbekchaga tarjima (faqat dastlabki 500 harf)
+        synopsis_uz = await translate_to_uz(synopsis_en[:500])
+        
+        # Emojilarni tayyorlash
+        nom_emoji = f'<tg-emoji emoji-id="{E_NOM}">📝</tg-emoji>'
+        sifat_emoji = f'<tg-emoji emoji-id="{E_SIFAT}">⭐</tg-emoji>'
+        tag_emoji = f'<tg-emoji emoji-id="{E_TAG}">🏷</tg-emoji>'
+        silent_emoji = f'<tg-emoji emoji-id="{E_SILENT}">🔇</tg-emoji>'
         
         caption = (
-            f"🎬 <b>Nomi:</b> {title} {diamond}\n"
-            f"⭐ <b>Reyting:</b> {score} {fire}\n\n"
-            f"📝 <b>Annotatsiya:</b> {anime.get('synopsis', '...')[:200]}..."
+            f"{nom_emoji} <b>Nomi:</b> {title} {silent_emoji}\n"
+            f"{sifat_emoji} <b>Reyting:</b> {score}\n\n"
+            f"{tag_emoji} <b>Annotatsiya:</b>\n<i>{synopsis_uz}</i>"
         )
         
-        await message.answer_photo(photo=image_url, caption=caption, parse_mode="HTML")
+        # Ulashish tugmasini qo'shish
+        kb = [[types.InlineKeyboardButton(text="Do'stlarga ulashish", switch_inline_query=title)]]
+        markup = types.InlineKeyboardMarkup(inline_keyboard=kb)
+        
+        await message.answer_photo(photo=image_url, caption=caption, parse_mode="HTML", reply_markup=markup)
         await msg.delete()
     else:
-        await msg.edit_text("Topilmadi ❌")
+        await msg.edit_text("Afsuski, bunday anime topilmadi. ❌")
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    print("Bot muvaffaqiyatli ishga tushdi...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
