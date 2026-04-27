94d2b5005123056d0bcc4fa51672b01815b06e0f
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-27 18:45:00 +0500

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,16 @@
+STRINGS = {
+    "uz": {
+        "settings_title": "⚙️ <b>Sozlamalar bo‘limi</b>",
+        "profile_not_found": "Profil topilmadi. Iltimos, /start bosing.",
+        "notify_status": "Bildirishnomalar",
+        "on": "YOQILGAN",
+        "off": "O‘CHIRILGAN"
+    },
+    "ru": {
+        "settings_title": "⚙️ <b>Раздел настроек</b>",
+        "profile_not_found": "Профиль не найден. Пожалуйста, нажмите /start.",
+        "notify_status": "Уведомления",
+        "on": "ВКЛЮЧЕНО",
+        "off": "ВЫКЛЮЧЕНО"
+    }
+}
