b19b50a5af0f65a266166a11ed6892f47c05099a
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-05-15 21:59:42 +0500

    Delete handlers directory

--- handlers/settings.py
+++
@@ -1,5 +0,0 @@
-from aiogram import Router
-
-router = Router()
-
-# Keyinchalik bu yerga sozlamalar mantiqini qo'shamiz
--- handlers/sevimlilar.py
+++
@@ -1,5 +0,0 @@
-from aiogram import Router
-
-router = Router()
-
-# Keyinchalik bu yerga sevimlilar mantiqini qo'shamiz
